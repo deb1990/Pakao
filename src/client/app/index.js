574316e6b1b05312d65a8b52344185979081e6b9
Debarshi Bhaumik <debarshi.bhaumik@pearson.com>
2016-05-18 02:13:38 +0530

    router

--- src/client/app/index.js
+++ src/client/app/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import {render} from 'react-dom';
 import Header from "./Header";
+import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router';
 
 class App extends React.Component {
     render() {
-        return <div><Header></Header><p> Hello React!</p></div>;
+        return <div>
+        <h1>App</h1>
+        <ul>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+        {this.props.children}
+      </div>;
     }
 }
 
-render(<App/>, document.getElementById('app'));
+
+class Nav extends React.Component {
+    render() {
+        return <div>nav</div>;
+    }
+}
+
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <Route path="about" component={Nav}/>
+    </Route>
+  </Router>
+), document.getElementById('app'))
+
+
+// render(<App/>, document.getElementById('app'));
