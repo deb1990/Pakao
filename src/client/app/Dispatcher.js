a63da35e5824abc1e67f5fcaa8c37e6f6ea78a0f
Debarshi Bhaumik <debarshi@compucorp.co.uk>
2016-06-18 02:17:50 +0530

    Flux added

---
+++ src/client/app/Dispatcher.js
@@ -0,0 +1,6 @@
+/**
+ * Created by debarshi on 18/6/16.
+ */
+import { Dispatcher } from "flux";
+
+export default new Dispatcher;
--- src/client/app/index.js
+++ src/client/app/index.js
@@ -2,14 +2,34 @@ import React from 'react';
 import {render} from 'react-dom';
 import Header from "./Header";
 import { Router, Route, Link, hashHistory, IndexRoute } from 'react-router';
+import * as actions from "./actions/actions";
+import demoStore from "./stores/demoStore";
+import dispatcher from "./Dispatcher";
 
 class App extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            todos: demoStore.getAll()
+        };
+        demoStore.on("change", function(){
+            this.setState({todos:demoStore.getAll()});
+        }.bind(this));
+    }
+    addTodo(){
+        dispatcher.dispatch({
+            type: "CREATE_TODO",
+            text: "Debarshi"
+        });
+    }
     render() {
         return <div>
         <h1>App</h1>
         <ul>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/head">Header</Link></li>
+            {this.state.todos.map(function(itm){ return <div>{itm.text}</div>})}
+            <button onClick={this.addTodo.bind(this)}>Add</button>
         </ul>
         {this.props.children}
       </div>;
